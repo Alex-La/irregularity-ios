6cf3b6f13481edde0c06e1cf9180be29d5534b3d
Alex-La <55190911+Alex-La@users.noreply.github.com>
2023-06-13 19:07:56 +0300

    Select favorite landmarks

--- Irregularity/Views/Landmarks/LandmarkDetail.swift
+++ Irregularity/Views/Landmarks/LandmarkDetail.swift
@@ -9,7 +9,12 @@ import SwiftUI
 import MapKit
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+          modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+      }
     
     var body: some View {
         ScrollView {
@@ -24,9 +29,11 @@ struct LandmarkDetail: View {
             
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -54,6 +61,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[1])
+        LandmarkDetail(landmark: ModelData().landmarks[1])
     }
 }
---
+++ Irregularity/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,44 @@
+//
+//  LandmarkList.swift
+//  Irregularity
+//
+//  Created by Alex Larionov on 13.06.2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filtredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filtredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
