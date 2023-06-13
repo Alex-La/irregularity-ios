99e275599b25f814d9aca40b407d84900a16ab6c
Alex-La <55190911+Alex-La@users.noreply.github.com>
2023-06-13 16:19:29 +0300

    Initial Commit

---
+++ Irregularity/IrregularityApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrregularityApp.swift
+//  Irregularity
+//
+//  Created by Alex Larionov on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct IrregularityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
